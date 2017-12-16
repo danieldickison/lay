0ea3aeb8682746e6c00bfa4eaa427542356e4e99
Daniel Dickison <git@danieldickison.com>
2017-12-16 10:45:05 -0500

    Add iOS webview app

---
+++ client/Looking At You/Looking At You/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Looking At You
+//
+//  Created by Daniel Dickison on 12/16/17.
+//  Copyright © 2017 Daniel Dickison. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    let HOST = "http://localhost:3000"
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loadingImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        let req = URLRequest(url: URL(string: "\(HOST)/tablettes/index")!)
+        webView.load(req)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("weview committed navigation \(navigation)")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("webview finished navigation \(navigation)")
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseIn], animations: {
+            self.loadingImage.alpha = 0
+        }, completion: {_ in
+            self.loadingImage.removeFromSuperview()
+        })
+    }
+}
