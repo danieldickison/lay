948c67918ece496c1cb22b84a86813b39959ef1b
Daniel Dickison <git@danieldickison.com>
2019-06-10 00:11:08 -0400

    Lint

--- android/app/src/main/java/com/danieldickison/lookingatyou/Downloader.java
+++ android/app/src/main/java/com/danieldickison/lookingatyou/Downloader.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
