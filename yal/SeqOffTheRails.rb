22930e7f658ac0cc502c597a7a67889d44c61070
Joe Holt <1596163+joe07734@users.noreply.github.com>
2019-06-21 11:18:35 -0400

    i

--- yal/SeqOffTheRails.rb
+++ yal/SeqOffTheRails.rb
@@ -34,7 +34,7 @@ class SeqOffTheRails
         puts "Profile..."
         debug_images = `find "#{DATABASE}/profile" -name "*" -print`.lines.find_all {|f| File.extname(f.strip) != ""}
         profile_image_names = {}
-        16.times do |i|
+        10.times do |i|
             begin
                 r = rand(debug_images.length)
                 f = debug_images.delete_at(r).strip
@@ -48,7 +48,40 @@ class SeqOffTheRails
                 retry
             end
         end
+
+        # Hard-coded for 6/21 
+        # Aislinn Curry
+        f = "#{DATABASE}/hard_profile/test.jpg"
+        name = "510-011-R02-OTR_profile.png"
+        GraphicsMagick.thumbnail(f, "#{MEDIA_PROFILE}/#{name}", 180, 180, "png")
+
+        # Cierra Cass
+        f = "#{DATABASE}/hard_profile/test.jpg"
+        name = "510-012-R02-OTR_profile.png"
+        GraphicsMagick.thumbnail(f, "#{MEDIA_PROFILE}/#{name}", 180, 180, "png")
+
+        # Kamala Sankaram
+        f = "#{DATABASE}/hard_profile/test.jpg"
+        name = "510-013-R02-OTR_profile.png"
+        GraphicsMagick.thumbnail(f, "#{MEDIA_PROFILE}/#{name}", 180, 180, "png")
+
+        # Anne Hiatt
+        f = "#{DATABASE}/hard_profile/test.jpg"
+        name = "510-014-R02-OTR_profile.png"
+        GraphicsMagick.thumbnail(f, "#{MEDIA_PROFILE}/#{name}", 180, 180, "png")
+
+        # Jason Cady
+        f = "#{DATABASE}/hard_profile/test.jpg"
+        name = "510-015-R02-OTR_profile.png"
+        GraphicsMagick.thumbnail(f, "#{MEDIA_PROFILE}/#{name}", 180, 180, "png")
+
+        # Nick Benacerraf
+        f = "#{DATABASE}/hard_profile/test.jpg"
+        name = "510-016-R02-OTR_profile.png"
+        GraphicsMagick.thumbnail(f, "#{MEDIA_PROFILE}/#{name}", 180, 180, "png")
+
         pbdata[:profile_image_names] = profile_image_names
+
 
         [["facebook images", MEDIA_FACEBOOK, "511", "Facebook", :facebook_image_names],
          ["instagram images", MEDIA_INSTAGRAM, "512", "Instagram", :instagram_image_names],
@@ -75,21 +108,61 @@ class SeqOffTheRails
         end
 
         tweets = [
-            {:profile => 1, :tweet => 'hi i ate a sandwich adn it was good'},
-            {:profile => 2, :tweet => 'look at me im on social media'},
-            {:profile => 3, :tweet => 'covfefe'},
-            {:profile => 4, :tweet => 'oneuoloenthlonglonglongtextstringwhathappens'},
-            {:profile => 5, :tweet => 'ユニコード'}
+            # {:profile => 1, :tweet => 'hi i ate a sandwich adn it was good'},
+            # {:profile => 2, :tweet => 'look at me im on social media'},
+            # {:profile => 3, :tweet => 'covfefe'},
+            # {:profile => 4, :tweet => 'oneuoloenthlonglonglongtextstringwhathappens'},
+            # {:profile => 5, :tweet => 'ユニコード'},
+
+            # Hard-coded for 6/21
+            {:profile => 11, :tweet => "Need some late night Chekhov, tinged with tequila? 22:50 @greensidevenue. #edfringe"},
+            {:profile => 11, :tweet => "@NYTW79 i've got no words to describe how much @OnceMusical moved me this afternoon . . . it was just a tremendously beautiful experience."},
+            {:profile => 11, :tweet => "apparently my twitter got hacked. disregard anything i tweeted sent you today."},
+            {:profile => 11, :tweet => "my subway woes went to a whole new level today . . . and i resorted to taking a cab. who am i?"},
+
+            {:profile => 12, :tweet => "The amount of text conversations I’ve ended with “oaky” is concerning. #oaky #likewine #butnot"},
+            {:profile => 12, :tweet => "There is nothing more disappointing than biting into a chocolate muffin to find it has a cherry center *shudders* #thisiswhyihavetrustissues"},
+            {:profile => 12, :tweet => "When in doubt, have a margarita. #28andblossoming"},
+
+            {:profile => 13, :tweet => "Modern life is me and my Lyft driver silently grooving together to the Pixies without have spoken a word..."},
+
+            {:profile => 14, :tweet => "Yowza. I'm feeling a brain storm coming on!"},
+            {:profile => 14, :tweet => "Trying out this whole self-promotion thang."},
+            {:profile => 14, :tweet => "Thank you @lisapeyton for including me in this great piece for VentureBeat on where immersive tech could take us.  So much fun to ruminate on my most pie-in-the-sky prediction!: https://lnkd.in/d_dtTRE "},
+
+            {:profile => 15, :tweet => "Last week I released Buick City, 1:00 AM. It's a podcast opera about a woman time-traveling to 1984 to prevent the murder of her father, an auto-worker in Flint, Michigan. Episode 2 just came out today. #iTunes"},
+            {:profile => 15, :tweet => "'She had last smoked from this pack around December of 1982, and this baby was staler than the ERA in the Illinois state senate.' @StephenKing, It, 1985"},
+
+            {:profile => 16, :tweet => "I'm back in the twitter twatter! Hoping to increase my rate of posting once every 5 years."},
+            {:profile => 16, :tweet => "Also, my iPhone really gets me tonight. This pizza is genius."},
+            {:profile => 16, :tweet => "Theater is a competitive sport. #lilysrevenge"},
+            {:profile => 16, :tweet => "You can be ugly and stupid as long as you have a big shaft. -spam email"},
         ]
 
         facebooks = [
-            {:photo => 1, :caption => 'this is a caption'},
-            {:photo => 2, :caption => 'look at you your on social media'},
+            {:photo => 1, :caption => ""},
+            {:photo => 2, :caption => ""},
+            {:photo => 3, :caption => ""},
+            {:photo => 4, :caption => ""},
+            {:photo => 5, :caption => ""},
+            {:photo => 6, :caption => ""},
+            {:photo => 7, :caption => ""},
+            {:photo => 8, :caption => ""},
+            {:photo => 9, :caption => ""},
+            {:photo => 10, :caption => ""},
         ]
 
         instagrams = [
-            {:photo => 3, :caption => 'this is an insta'},
-            {:photo => 4, :caption => 'look at you your on instagram'},
+            {:photo => 1, :caption => ""},
+            {:photo => 2, :caption => ""},
+            {:photo => 3, :caption => ""},
+            {:photo => 4, :caption => ""},
+            {:photo => 5, :caption => ""},
+            {:photo => 6, :caption => ""},
+            {:photo => 7, :caption => ""},
+            {:photo => 8, :caption => ""},
+            {:photo => 9, :caption => ""},
+            {:photo => 10, :caption => ""},
         ]
 
         pbdata[:tweets] = tweets
